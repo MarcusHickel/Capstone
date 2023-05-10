4f50d7386fdb0b529ab43cf2d62b755cce1ea6ea
Marcus <marcus.hickel@gmail.com>
2023-05-10 15:57:13 +1000

    Added launch script for controller

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ syntax for launch files:
 ros2 launch [packagename] [launcherfile]
 ```
 
-3. Launch the Launch `joint_state_publisher_gui` with 
+3. Launch the `joint_state_publisher_gui` with 
 ```
 ros2 run joint_state_publisher_gui joint_state_publisher_gui
 ```
@@ -41,3 +41,42 @@ rviz2
 - Set fixed frame to 'world'
 - Add a 'RobotModel' display, with the topic set to '/robot_description', and alpha set to 0.8
 - Add a 'TF' display with names enabled.
+
+ToDo
+Control Rocket
+    Teleop?
+    https://control.ros.org/master/doc/ros2_controllers/doc/controllers_index.html#available-controllers
+    https://control.ros.org/master/doc/ros2_control_demos/example_4/doc/userdoc.html
+    https://control.ros.org/master/doc/ros2_control_demos/example_1/doc/userdoc.html <- 
+
+    http://wiki.ros.org/robot_mechanism_controllers/JointPositionController
+    https://www.rosroboticslearning.com/ros-control
+
+
+        Current issue: 
+
+        Controller mananger wasnt spawning 
+        running ctrl.launch.py 
+        [gzserver-1] [WARN] [1683695189.182994074] [gazebo_ros2_control]: There is no joint or sensor available
+        [gzserver-1] [FATAL] [1683695189.183028701] [gazebo_ros2_control]: Could not initialize robot simulation interface
+        Fixed? Not sure what is issue was, just started working
+
+
+        controller.yaml is failing. 
+        Fixed: config folder added to cmake 
+
+    Launch File
+    Controllers yaml
+    URDF file
+        Description
+        ros2_control tag
+    RViz config
+    Test nodes
+
+Control Platform
+Impletment blob find
+Changing FOV on camera
+
+Issues:
+Running joint state publisher gui breaks rviz ability to see camera and doesnt control gazebo
+TF_OLD_DATA
---
+++ dev_ws/src/trackingplatform/launch/ctrl.launch.py
@@ -0,0 +1,81 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'trackingplatform'
+    file_subpath = 'description/robot.urdf.xacro'
+
+
+    # Use xacro to process the file
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        )
+
+
+    # Configure the node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw,
+        'use_sim_time': True}] # add other parameters here if required
+    )
+
+
+    # Run the spawner node from the gazebo_ros package.
+    spawn_entity = Node(
+                        package='gazebo_ros', 
+                        executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                '-entity', 'my_bot'],
+                        output='screen'
+                        )
+    
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+    )
+
+
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_broad_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+
+    # Run the node
+    return LaunchDescription([
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity,
+        joint_broad_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner
+    ])
+
